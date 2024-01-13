2fbf72fbbf00d0889beca5dc107d21b56e427f7b
Sanjay Kumar Sah <group.ansopedia@gmail.com>
2024-01-13 21:18:32 +0530

    fix(tests): ensure test onlu run on src files

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts', 'json', 'node'],
-  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  modulePathIgnorePatterns: ['<rootDir>/build'],
   roots: ['<rootDir>'],
   setupFiles: ['dotenv/config'],
   testEnvironment: 'jest-environment-node',
