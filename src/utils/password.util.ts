12808c7f84da26611aa820a6d8f905454c2c2861
Sanjay Kumar Sah <sanjaykumar.sah@zuru.com>
2024-10-21 17:05:55 +0530

    chore(deps): replace bcrypt with bcryptjs

--- src/utils/password.util.ts
+++ src/utils/password.util.ts
@@ -1,11 +1,11 @@
-import * as bcrypt from "bcrypt";
+import * as bcryptjs from "bcryptjs";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10;
-  const salt = await bcrypt.genSalt(saltRounds);
-  return bcrypt.hash(password, salt);
+  const salt = await bcryptjs.genSalt(saltRounds);
+  return bcryptjs.hash(password, salt);
 };
 
 export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
-  return await bcrypt.compare(password, hashedPassword);
+  return await bcryptjs.compare(password, hashedPassword);
 };
