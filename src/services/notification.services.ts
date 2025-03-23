d232d2569c33839e74226f6034c2f1e0ff21f4d3
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2025-03-23 14:14:24 +0530

    feat: improve error handling and structured logging in microservices

--- src/services/notification.services.ts
+++ src/services/notification.services.ts
@@ -1,37 +1,46 @@
-import { envConstants } from "@/constants";
-import { logger } from "@/utils";
+import { ErrorTypeEnum, envConstants } from "@/constants";
 
+import { errorLogger } from "../utils/logger";
 import { EmailNotification } from "./notification.validation";
 
 const sendEmail = async (body: EmailNotification) => {
+  // Skiping sending notification for test environment
+  if (envConstants.NODE_ENV === "test") return;
+
+  let response: Response | undefined;
   try {
-    const response = await fetch(`${envConstants.NOTIFICATION_SERVICE_BASE_URL}/api/v1/emails`, {
+    response = await fetch(`${envConstants.NOTIFICATION_SERVICE_BASE_URL}/api/v1/emails`, {
       method: "POST",
       body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json",
         origin: envConstants.USER_SERVICE_BASE_URL,
       },
+      // Add timeout (optional but recommended)
+      signal: AbortSignal.timeout(5000),
     });
-
-    if (!response.ok) {
-      const responseBody = await response.text();
-      logger.error({
-        message: "Email notification service responded with an error",
-        status: response.status,
-        statusText: response.statusText,
-        responseBody,
-        requestBody: body,
-      });
-      throw new Error(`Notification service error: ${response.status} ${response.statusText}`);
-    }
   } catch (error) {
-    logger.error({
-      message: "Exception while sending email",
-      error,
+    if (error instanceof Error) {
+      if (error.message === "fetch failed") {
+        throw new Error(ErrorTypeEnum.Enum.NOTIFICATION_SERVICE_UNAVAILABLE);
+      }
+    } else {
+      // TODO: fix with appropriate error message
+      throw new Error(ErrorTypeEnum.Enum.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  if (response && !response.ok) {
+    const responseBody = await response.json();
+    errorLogger.error({
+      message: "Email notification service responded with an error",
+      status: response.status,
+      statusText: response.statusText,
+      responseBody,
       requestBody: body,
     });
-    throw error;
+
+    throw new Error(ErrorTypeEnum.Enum.NOTIFICATION_SERVICE_MISCONFIGURED);
   }
 };
 
