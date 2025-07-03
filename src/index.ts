9d2738f2c2c3dc486d5d541336865ebe024f4928
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2025-07-03 12:24:01 +0530

    refactor: remove initial setup flag and related code
    The INITIAL_SETUP_DONE flag and associated initialization code were removed as they are no longer needed in the application workflow. This simplifies the startup process and removes unnecessary configuration.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,10 @@ import { connectDB } from "./config";
 import { envConstants } from "./constants";
 import { startServer } from "./server";
 import { logger } from "./utils";
-import { setupInitialRolesAndPermissions, setupInitialUserRole } from "./utils/initialize-db";
 
 (async () => {
   try {
     await connectDB();
-    if (!envConstants.INITIAL_SETUP_DONE) {
-      await setupInitialRolesAndPermissions();
-      await setupInitialUserRole();
-    }
     await startServer(envConstants.APP_PORT);
   } catch (error) {
     logger.error("Failed to setup initial data:", error);
